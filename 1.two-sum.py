49b5cdf96096502ee8a7b332e4e12fedd452d064
gg9096 <1002242149@qq.com>
2019-05-21 19:14:21 +0800

    program1

--- 1.two-sum.py
+++ 1.two-sum.py
@@ -4,6 +4,12 @@
 # [1] 两数之和
 #
 
+"""
+idea   
+循环遍历nums,使用一个哈希表来存储<每个值所需要匹配的值，自身下标>，
+然后依次遍历，每次nums中的值都会与哈希表做包含判断，自己的index()方法耗费太多time了
+"""
+
 #first
 # class Solution(object):
 #     def twoSum(self, nums, target):
@@ -13,6 +19,7 @@
 #                     sum = nums[index1] + nums[index2]
 #                     if(sum == target):
 #                         return [index1,index2]
+
 
 # second                 
 class Solution(object):
