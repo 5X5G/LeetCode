b494a89510d9bc9008e0595c28bf24b372fec064
gg9096 <1002242149@qq.com>
2019-05-21 13:36:23 +0800

    program1

--- 1.两数之和.py
+++ 1.两数之和.py
@@ -3,11 +3,21 @@
 #
 # [1] 两数之和
 #
+
+#first
+# class Solution(object):
+#     def twoSum(self, nums, target):
+#         for index1 in range(len(nums)):
+#             for index2 in range(len(nums)):
+#                 if(index2 > index1 ):
+#                     sum = nums[index1] + nums[index2]
+#                     if(sum == target):
+#                         return [index1,index2]
+
+# second                 
 class Solution(object):
     def twoSum(self, nums, target):
-        for index1 in range(len(nums)):
-            for index2 in range(len(nums)):
-                if(index2 > index1 ):
-                    sum = nums[index1] + nums[index2]
-                    if(sum == target):
-                        return [index1,index2]        
+        for index in range(len(nums)):
+            num = nums[index]
+            if target-num in nums and nums.index(target-num) != index:
+                return [index,nums.index(target-num)]
